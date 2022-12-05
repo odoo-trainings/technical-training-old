8691b6b207be035d2e74af30e0bd449a66eacba2
“fsrs-odoo” <fsrs@odoo.com>
2022-12-05 18:36:17 +0000

    [ADD] Odoo Academy Module and Icon.

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'assets': {},
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
