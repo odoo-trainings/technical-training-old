02115ca65037057a0c4dcec2ec2b330753658cfc
“fsrs-odoo” <fsrs@odoo.com>
2022-12-06 18:05:49 +0000

    [ADD] New Library Module

---
+++ library/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Library',
+    'version': '0.1',
+    'summary': 'Odoo Application to manage its Library',
+    'description': """
+        Odoo Application to manage its Library:
+            - Books
+            -
+            -
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    
+    'assets': {},
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
