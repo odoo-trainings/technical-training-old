06e22f01a3f9e505d164f43c2274f32fa96e808c
“fsrs-odoo” <fsrs@odoo.com>
2022-12-06 18:08:09 +0000

    [ADD] New Motoclub Module

---
+++ motoclub/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Motoclub',
+    'version': '0.1',
+    'summary': 'Odoo Application to manage a Motoclub',
+    'description': """
+        Odoo Application to manage a Motoclub:
+            - Club
+            -
+            -
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    
+    'assets': {},
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
