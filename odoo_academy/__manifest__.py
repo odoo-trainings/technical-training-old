0edba455fa58966aca8cf7e844ea12cad1a5c8db
“fsrs-odoo” <fsrs@odoo.com>
2023-01-20 00:35:44 +0000

    [ADD] Course Views.

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -19,6 +19,7 @@
         'security/odoo_academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
+        'views/course_views.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
