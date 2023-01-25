802eaa9a15f05592b4cd0b4b7ebb566d4e649617
“fsrs-odoo” <fsrs@odoo.com>
2023-01-25 23:47:39 +0000

    [UPD] Add comments to Session Model

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -6,29 +6,25 @@ class Session(models.Model):
     _name = 'academy.session'
     _description = 'Session Info'
 
+    name = fields.Char(string='Title', related='course_id.name')
     course_id = fields.Many2one(comodel_name='academy.course',
                                string='Course',
                                ondelete='cascade',
                                required=True)
-
-    name = fields.Char(string='Title', related='course_id.name')
-
     instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
-
     student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
     
     # Calendar View Fields
     start_date = fields.Date(string="Start Date",
                             default=fields.Date.today)
-
     duration = fields.Integer(string='Session Days',
                              default=1)
-
     end_date = fields.Date(string='End Date',
                           compute='_compute_end_date',
                           inverse='_inverse_end_date',
                           store=True)
     
+    # End Date and Duration Compute Methods
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
